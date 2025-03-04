2f033b49933a194f9b6e3d291646e2f179eeb367
Anyi-zheng <zhenghks@outlook.com>
2025-03-04 16:19:50 +0800

    清理代码

--- internal/response/my_info_response.go
+++ internal/response/my_info_response.go
@@ -12,13 +12,3 @@ type MyInfoResponse struct {
 	InviteCode    string   `json:"invite_code"`    // 邀请码
 	HasBound      bool     `json:"has_bound"`      // 是否已绑定
 }
-
-// UserInfo 表示用户的基本信息。
-// @Description 包含用户 ID、地址、昵称、头像和描述等信息。
-type UserInfo struct {
-	UserID      string `json:"user_id"`     // 用户 ID
-	Address     string `json:"address"`     // 用户地址
-	Nickname    string `json:"nickname"`    // 用户昵称
-	Avatar      string `json:"avatar"`      // 用户头像
-	Description string `json:"description"` // 用户描述
-}
