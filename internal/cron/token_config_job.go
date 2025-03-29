fa919d53d2fab0cabf4ab94df8f4b67d4af00cb7
chunshankong <980408132@qq.com>
2025-03-29 18:25:28 +0800

    fix job

--- internal/cron/token_config_job.go
+++ internal/cron/token_config_job.go
@@ -1,7 +1,6 @@
 package cron
 
 import (
-	"encoding/json"
 	"time"
 
 	"game-fun-be/internal/constants"
@@ -11,7 +10,7 @@ import (
 )
 
 const (
-	TokenConfigTTL = 10 * time.Minute // TokenConfig在Redis中的过期时间
+	TokenConfigTTL = 6 * time.Minute // TokenConfig在Redis中的过期时间
 )
 
 // SyncTokenConfigToRedis 将TokenConfig数据同步到Redis
@@ -26,15 +25,8 @@ func SyncTokenConfigToRedis() error {
 		return err
 	}
 
-	// 2. 将TokenConfig数据序列化为JSON
-	tokenConfigsJSON, err := json.Marshal(tokenConfigs)
-	if err != nil {
-		util.Log().Error("序列化TokenConfig数据失败: %v", err)
-		return err
-	}
-
-	// 3. 将数据存储到Redis
-	if err := redis.Set(constants.TokenConfigRedisKey, string(tokenConfigsJSON), TokenConfigTTL); err != nil {
+	// 2. 将数据存储到Redis
+	if err := redis.Set(constants.TokenConfigRedisKey, tokenConfigs, TokenConfigTTL); err != nil {
 		util.Log().Error("存储TokenConfig数据到Redis失败: %v", err)
 		return err
 	}
